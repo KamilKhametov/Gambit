d85c45bd00bbebc5aad6f14245303181cbb619f2
KamilKhametov <khametovkamil93@gmail.com>
2020-11-05 17:18:26 +0300

    Create Shared Preferences for buttonBasket and buttonPlus

--- app/src/main/java/com/example/gambit/MyAdapter.java
+++ app/src/main/java/com/example/gambit/MyAdapter.java
@@ -1,5 +1,7 @@
 package com.example.gambit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,8 @@ import java.util.List;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     private List<ResponseData> responseDataList;
+    private SharedPreferences preferences;
+    private SharedPreferences preferencesPlus;
 
     public MyAdapter( List<ResponseData> responseDataList ) {
         this.responseDataList=responseDataList;
@@ -29,6 +33,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public MyAdapter.ViewHolder onCreateViewHolder( @NonNull ViewGroup parent, int viewType ) {
         View view =LayoutInflater.from ( parent.getContext () ).inflate ( R.layout.layout_item, parent, false );
         ViewHolder vh = new ViewHolder ( view );
+        if(preferences == null){
+            preferences = parent.getContext ().getSharedPreferences ( "NICE", Context.MODE_PRIVATE );
+        }
+        if (preferencesPlus == null){
+            preferencesPlus = parent.getContext ().getSharedPreferences ( "NICE_PLUS", Context.MODE_PRIVATE );
+        }
         return vh;
     }
 
@@ -51,6 +61,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         private Button btnMinus;
         private TextView textSum;
         private Button btnBasket;
+        private ResponseData currentResponseData;
+        boolean s;
+        boolean plus;
 
 
         public ViewHolder( @NonNull View itemView ) {
@@ -63,9 +76,31 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             textSum = itemView.findViewById ( R.id.textSum );
             btnBasket = itemView.findViewById ( R.id.btnBasket );
 
+            btnBasket.setOnClickListener ( new View.OnClickListener () {
+                @Override
+                public void onClick( View view ) {
+                    if(preferences.getBoolean ( String.valueOf ( currentResponseData.getId () ), false)){
+                        btnBasket.setVisibility ( View.VISIBLE );
+                        saveDataBtnBasket ( String.valueOf ( currentResponseData.getId () ), false );
+                        btnPlus.setVisibility ( View.VISIBLE );
+                        saveDataPlus ( String.valueOf ( currentResponseData.getId () ), false );
+                    }else {
+                        btnBasket.setVisibility ( View.GONE );
+                        saveDataBtnBasket ( String.valueOf ( currentResponseData.getId () ), true );
+                        btnPlus.setVisibility ( View.VISIBLE );
+                        saveDataPlus ( String.valueOf ( currentResponseData.getId () ), true );
+
+
+                    }
+                }
+            } );
+
         }
 
         public void bind( ResponseData responseData ) {
+            currentResponseData = responseData;
+            s = preferences.getBoolean ( String.valueOf ( currentResponseData.getId () ), true );
+            plus = preferencesPlus.getBoolean ( String.valueOf ( currentResponseData.getId () ), true );
             // get image from url and set in ImageView
             String imageUrl = responseData.getImage ();
             Picasso.with ( itemView.getContext () )
@@ -76,6 +111,34 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             // setting price on the button text
             btnPrice.setText ( String.valueOf ( responseData.getPrice () + "₽" ));
 
+
+            // Кнопка Basket при входе в приложение
+            if(preferences.getBoolean ( String.valueOf ( currentResponseData.getId () ), false )){
+                btnBasket.setVisibility ( View.GONE );
+            }else {
+                btnBasket.setVisibility ( View.VISIBLE );
+            }
+
+            // Кнопка Plus при входе в приложение
+            if(preferencesPlus.getBoolean ( String.valueOf ( currentResponseData.getId () ), false )){
+                btnPlus.setVisibility ( View.VISIBLE );
+            }else {
+                btnPlus.setVisibility ( View.GONE );
+            }
+
+
+        }
+
+        public void saveDataBtnBasket( String id, boolean dataToSave){
+            SharedPreferences.Editor editor = preferences.edit ();
+            editor.putBoolean ( id, dataToSave );
+            editor.apply ();
+        }
+
+        public void saveDataPlus(String id, boolean dataToSave){
+            SharedPreferences.Editor editor = preferencesPlus.edit ();
+            editor.putBoolean ( id, dataToSave );
+            editor.apply ();
         }
     }
 
