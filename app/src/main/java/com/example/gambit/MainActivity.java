3a6a5d7eb8c1eec9817873916d75fbd9c21cec00
KamilKhametov <khametovkamil93@gmail.com>
2020-11-11 18:11:51 +0300

    Changed layout_item.xml a bit Added a new font and moved the buttons. In the MainActivity.java class, added a test swipe for the RecyclerView items)

--- app/src/main/java/com/example/gambit/MainActivity.java
+++ app/src/main/java/com/example/gambit/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.gambit;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.example.gambit.API.APIConfig;
@@ -14,6 +18,7 @@ import com.example.gambit.API.ResponseData;
 
 import java.util.List;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,19 +38,23 @@ public class MainActivity extends AppCompatActivity {
         setActionBarTitle ();
 
         // Call request from API
-        APIService apiService =APIServiceConstructor.CreateService ( APIService.class );
-        Call<List<ResponseData>> call = apiService.getDataGambit ( APIConfig.PAGE );
+        APIService apiService=APIServiceConstructor.CreateService ( APIService.class );
+        Call<List<ResponseData>> call=apiService.getDataGambit ( APIConfig.PAGE );
         call.enqueue ( new Callback<List<ResponseData>> () {
             @Override
             public void onResponse( Call<List<ResponseData>> call, Response<List<ResponseData>> response ) {
-                if (response.isSuccessful ()){
-                    List<ResponseData> responseData = response.body ();
+                if (response.isSuccessful ()) {
+                    List<ResponseData> responseData=response.body ();
 
-                    recyclerView = findViewById ( R.id.recyclerView );
+                    recyclerView=findViewById ( R.id.recyclerView );
                     recyclerView.setHasFixedSize ( true );
-                    myAdapter = new MyAdapter ( responseData );
+                    myAdapter=new MyAdapter ( responseData );
                     recyclerView.setLayoutManager ( layoutManager );
                     recyclerView.setAdapter ( myAdapter );
+
+                    // Swipe method go!
+                    swipeInRecyclerView ();
+
                 }
             }
 
@@ -55,12 +64,55 @@ public class MainActivity extends AppCompatActivity {
             }
         } );
 
-
     }
 
-    private void setActionBarTitle(){
+    private void setActionBarTitle() {
         getSupportActionBar ().setDisplayOptions ( ActionBar.DISPLAY_SHOW_CUSTOM );
         getSupportActionBar ().setCustomView ( R.layout.action_bar_layout );
+    }
+
+    private void swipeInRecyclerView() {
+        // Swipe for RecyclerView
+        ItemTouchHelper.SimpleCallback callback=new ItemTouchHelper.SimpleCallback ( 0, ItemTouchHelper.LEFT ) {
+            @Override
+            public boolean onMove( RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target ) {
+                return false;
+            }
+
+            // Что делать при свайпе: добавить в архив, либо удалить item`ы
+            @Override
+            public void onSwiped( RecyclerView.ViewHolder viewHolder, int direction ) {
+                // Возвращает свайп обратно
+                myAdapter.notifyItemChanged ( viewHolder.getAdapterPosition () );
+
+            }
+
+            // swipe draw method
+            @Override
+            public void onChildDraw( Canvas c, RecyclerView recyclerView,
+                                     RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                     int actionState, boolean isCurrentlyActive ) {
+                new RecyclerViewSwipeDecorator.Builder ( c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive )
+                        .addSwipeLeftBackgroundColor ( ContextCompat.getColor ( MainActivity.this, R.color.recycler_view_item_swipe_left_background ) )
+                        .addSwipeLeftActionIcon ( R.drawable.ic_like )
+                        .setSwipeLeftLabelColor ( Color.WHITE )
+                        .create ()
+                        .decorate ();
+
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    float width=(float) viewHolder.itemView.getWidth ();
+                    float alpha= 30.0f - Math.abs ( dX ) / width;
+                    viewHolder.itemView.setAlpha ( alpha );
+                    viewHolder.itemView.setTranslationX ( dX );
+
+                } else {
+                    super.onChildDraw ( c, recyclerView, viewHolder, dX, dY,
+                                        actionState, isCurrentlyActive );
+                }
+            }
+        };
+        ItemTouchHelper itemTouchHelper=new ItemTouchHelper ( callback );
+        itemTouchHelper.attachToRecyclerView ( recyclerView );
     }
 
 }
